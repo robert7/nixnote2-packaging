d0ab3859e98abecf334c635e0de463d33ee20a98
Robert Spiegel <r.spiegel@netconomy.net>
2018-09-13 14:26:09 +0200

    Adjusted build helpers

---
+++ development/clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DEV_DIR=`dirname $0`
+. ${DEV_DIR}/common-include.sh
+
+for i in build* ; do
+  if [ -d $i ] ; then
+     echo clean: removing directory $i
+     rm -rf $i || error_exit "rm directory \"$i\""
+  fi
+  if [ -f $i ] ; then
+     echo Removing file $i || error_exit "rm file \"$i\""
+     rm -f $i
+  fi
+done
+
+echo clean OK
--- development/launchpad-build-tidy-recipe-locally.sh
+++ development/launchpad-build-tidy-recipe-locally.sh
@@ -3,5 +3,7 @@
 DEV_DIR=`dirname $0`
 . ${DEV_DIR}/common-include.sh
 
-git-build-recipe --allow-fallback-to-native --distribution bionic ./tidy-html5/launchpad-recipe
+./development/clean.sh
+
+git-build-recipe --allow-fallback-to-native --distribution bionic ./tidy-html5/launchpad-recipe build-tidy-html5
 
