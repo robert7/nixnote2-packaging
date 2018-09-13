fbe89d981bbb5a7497dfffa12294d3e3ae1392c8
Robert Spiegel <r.spiegel@netconomy.net>
2018-09-13 21:18:38 +0200

    Adjusted build helpers

---
+++ development/tidy56-dh-seqs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# this is approximation of test run for debian "rules"
+# convenience helper only
+# its not really exact, but may catch some mistakes
+# it must be run *after* running recipe & any local changes must be copied manually
+
+CDIR=`pwd`
+echo Current dir: $CDIR
+DEV_DIR=`dirname $0`
+cd ${DEV_DIR}/..
+PROJ_DIR=`pwd`
+echo Project dir: $PROJ_DIR
+. ${PROJ_DIR}/development/common-include.sh
+
+cd build-tidy-html56/launchpad-recipe
+
+dh clean --without=build-stamp                        || error_exit "dh clean"
+dh build-arch --without=build-stamp                   || error_exit "dh build-arch"
+fakeroot dh install-arch --without=build-stamp        || error_exit "dh install-arch"
+
+cd $CDIR
+
+
